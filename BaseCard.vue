5e806945ec9c4152bf601708555fd9155b70be96
David <david.scheu97@gmail.com>
2026-09-24 19:04:36 +0200

    init commit

---
+++ BaseCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="card">
+    <div v-if="$slots.header" class="card-header">
+      <slot name="header" />
+    </div>
+    <div class="card-body">
+      <slot />
+    </div>
+  </div>
+</template>
